45e7d7104ac097454300d642043eafc6fec919c8
MikeK1639 <126822125+MikeK1639@users.noreply.github.com>
2023-04-30 10:23:35 -0400

    installed necessary mods and added schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,50 @@
-DROP DATABASE IF EXISTS employee_db;
-CREATE DATABASE employee_db;
-USE employee_db; 
+CREATE DATABASE employee_tracker_db;
+
+USE employee_tracker_db;
+
+CREATE TABLE departments (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10, 2) NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+  FOREIGN KEY (manager_id) REFERENCES employees(id)
+);
+
+INSERT INTO departments (name)
+VALUES ("Sales"),
+       ("Engineering"),
+       ("Finance"),
+       ("Legal");
+      
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Sales Lead",100000, 1),
+       ("Salesperson",80000, 1),
+       ("Lead Engineer",150000, 2),
+       ("Legal Team Lead",250000, 4),
+       ("Lawyer",190000, 4);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1, null),
+       ("Mike", "Chan", 2, 1),
+       ("Ashley", "Rodriguez", 3, null),
+       ("Kevin", "Tupik", 5, 3),
+       ("Sarah", "Lourd", 4, null),
+       ;
